e84fb149c52ffbe081d95f3ab0efe7f5850c6456
Falco Bethke <fbethke@outlook.de>
2024-06-08 10:46:56 +0200

    updates (Aero ohne Verluste)

---
+++ stva.js
@@ -0,0 +1,17 @@
+fetch("https://tevis.krzn.de/tevisweb700/location?mdt=95&select_cnc=1&cnc-1409=1", {
+  "headers": {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "en-US,en;q=0.9,de;q=0.8,de-DE;q=0.7",
+    "sec-ch-ua": "\"Chromium\";v=\"124\", \"Microsoft Edge\";v=\"124\", \"Not-A.Brand\";v=\"99\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "upgrade-insecure-requests": "1",
+    "cookie": "tvo_session=8ohbmjvs96nscgrd83b8m0vi2b; tvo_cookie_accept=0; tvo_map_agreement=1"
+  },
+  "referrerPolicy": "same-origin",
+  "body": null,
+  "method": "GET"
+});
